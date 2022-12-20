3b7754a456ce77384ee2f28745ccafbfabbe9e4f
xxx-learner <82637498+xxx-learner@users.noreply.github.com>
2022-12-20 22:10:40 +0300

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,43 @@
+''' space is a string'''
+'''capitalize(),COUNT().string.whitespace()'''
+
+
+def space():
+    print("please enter the text:")
+    text=input()
+    
+   
+    if text.count(" "):
+        
+
+
+
+
+
+"what am ı wanting? I dont wanna count space"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        """ I want to three more spaces add word but I don'T wanna make much effort"""
+        
+
+
+    
+
+''' space is a string.Now I search string'''
+
+
+                          
+        
+    
